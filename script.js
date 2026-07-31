7e5cb6cdc259d8212045c39fc6657c9d96e5b73b
ROHIT  RANA <83514950+rohittrana@users.noreply.github.com>
2026-07-31 18:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name;
+		this.age;
+	}
+	get_name(){
+		return this._name;
+	}
+	set name(name){
+		this._name=name;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(age){
+		this._age=age;
+	}
+}
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
